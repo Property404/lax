f1e6c9b19d6749785594b7e86ef84ddc50f1902c
Dagan Martinez <dmartinez@starry.com>
2022-01-29 14:02:35 -0500

    Refactor argument parser

--- src/argparser.rs
+++ src/argparser.rs
@@ -3,238 +3,173 @@
 //!
 //! Methods in this module exit upon failure.
 
-use std::process;
+#[macro_export]
+macro_rules! BuildArgumentParser {
+    (
+        name: $name:literal,
+        description: $description:literal,
+        usage: $usage:literal,
 
-const HELP_FLAG: &str = "!HELP";
-const VERSION_FLAG: &str = "!VERSION";
-
-/// A command line flag.
-pub struct Flag {
-    /// What the client refers to this flag as
-    name: &'static str,
-
-    /// Description for help text
-    description: Option<&'static str>,
-
-    /// --long style flag
-    long: Option<&'static str>,
-
-    /// -sh -or -t style flag
-    short: Option<char>,
-
-    /// Has this flag been matched yet?
-    has: bool,
-}
-
-pub struct ArgumentParser {
-    /// Name of this program
-    name: &'static str,
-
-    /// Description of this program
-    description: &'static str,
-
-    /// Usage text of this program
-    usage: &'static str,
-
-    /// All possible flags the user can pass
-    flags: Vec<Flag>,
-}
-
-impl ArgumentParser {
-    /// Constructor.
-    pub fn new(name: &'static str, description: &'static str, usage: &'static str) -> Self {
-        ArgumentParser {
-            name,
-            description,
-            usage,
-            flags: Vec::new(),
+        flags: {
+            $(
+                #[doc = $flag_description:expr]
+                $flag: ident: ($short: literal, $long:literal)
+            ),*
         }
-        .add_flag(
-            Flag::new(HELP_FLAG)
-                .set_description("Prints help information")
-                .set_long("--help")
-                .set_short('h'),
-        )
-        .add_flag(
-            Flag::new(VERSION_FLAG)
-                .set_description("Print version info and exit")
-                .set_long("--version"),
-        )
-    }
+    ) => {
+        BuildArgumentParser!{@
+            $name,
+            $description,
+            $usage,
 
-    /// Add a processable flag
-    pub fn add_flag(mut self, flag: Flag) -> Self {
-        self.flags.push(flag);
-        self
-    }
+            $(
+                #[doc = $flag_description]
+                $flag:  ($short, $long),
+            )*
+            /// Print help information
+            help: ('h', "--help"),
+            /// Print version info and exit
+            version: ('V', "--version")
+        }
+    };
+    (@
+        $name:literal,
+        $description:literal,
+        $usage:literal,
 
-    /// Process a single argument. Determine what flag it's associated with and fail if there's no
-    /// associated flag.
-    fn process_argument(&mut self, argument: &str) {
-        let is_long = argument.starts_with("--");
+        $(
+            #[doc = $flag_description:expr]
+            $flag: ident: ($short: literal, $long:literal)
+        ),*
+    ) => {
+        #[derive(Default)]
+        pub struct ArgumentParser {
+            $(
+                $flag: bool
+            ),*
+        }
+        impl ArgumentParser {
+            /// Process a single argument. Determine what flag it's associated with and fail if there's no
+            /// associated flag.
+            fn process_argument(&mut self, argument: &str) {
+                let is_long = argument.starts_with("--");
 
-        if is_long {
-            for flag in &mut (self.flags) {
-                if flag.match_against_long(argument) {
+                if is_long {
+                    match argument {
+                        $(
+                           $long => { self.$flag = true }
+                        ),*
+                        _ => {
+                            eprintln!("Invalid flag '{}'", argument);
+                            std::process::exit(1);
+                        }
+                    };
                     return;
                 }
-            }
-            eprintln!("Invalid flag '{}'", argument);
-            process::exit(1);
-        }
 
-        for character in (&argument[1..]).chars() {
-            let mut matched = false;
-            for flag in &mut (self.flags) {
-                if flag.match_against_short(character) {
-                    matched = true;
+                for character in (&argument[1..]).chars() {
+                    match character {
+                        $(
+                           $short => { self.$flag = true }
+                        ),*
+                        _ => {
+                            eprintln!("Invalid flag '{}'", character);
+                            std::process::exit(1);
+                        }
+                    };
                 }
             }
-            if !matched {
-                eprintln!("Invalid flag '{}'", character);
-                process::exit(1);
+
+            /// Process a list of arguments up until the first non-flag is found,
+            /// then return the flagless part of the vector
+            pub fn process_arguments<'a>(&mut self, arguments: &'a [String]) -> &'a [String] {
+                // Very first argument is just the name, so skip it
+                let mut position: usize = 1;
+
+                for arg in &arguments[position..] {
+                    // Explicitly stop processing args
+                    if arg == "--" {
+                        position += 1;
+                        break;
+                    }
+
+                    if arg.starts_with('-') {
+                        self.process_argument(arg.as_str());
+                        position += 1;
+                        continue;
+                    };
+                    break;
+                }
+
+                if self.help {
+                    println!(
+                        "{}\n{}\n\nUSAGE:\n    {}\n\nFLAGS:\n",
+                        $name, $description, $usage
+                    );
+
+                    $(
+                        println!("    -{}, {:15}{}", $short, $long, $flag_description);
+                    )*
+
+                    std::process::exit(0);
+                };
+
+                if self.version {
+                    println!("{} {}", $name, env!("CARGO_PKG_VERSION"));
+                    std::process::exit(0);
+                };
+
+                &arguments[position..]
             }
         }
-    }
-
-    /// Process a list of arguments up until the first non-flag is found,
-    /// then return the flagless part of the vector
-    pub fn process_arguments<'a>(&mut self, arguments: &'a [String]) -> &'a [String] {
-        // Very first argument is just the name, so skip it
-        let mut position: usize = 1;
-
-        for arg in &arguments[position..] {
-            // Explicitly stop processing args
-            if arg == "--" {
-                position += 1;
-                break;
-            }
-
-            if arg.starts_with('-') {
-                self.process_argument(arg.as_str());
-                position += 1;
-                continue;
-            };
-            break;
-        }
-
-        if self.has(HELP_FLAG) {
-            println!("{}", self.help());
-            process::exit(0);
-        };
-
-        if self.has(VERSION_FLAG) {
-            println!("{} {}", self.name, env!("CARGO_PKG_VERSION"));
-            process::exit(0);
-        };
-
-        &arguments[position..]
-    }
-
-    /// Returns true if a particular flag has been matched.
-    ///
-    /// # Panics
-    /// Panics if flag doesn't exist.
-    pub fn has(&self, name: &str) -> bool {
-        for flag in &(self.flags) {
-            if flag.name == name {
-                return flag.has_match();
-            }
-        }
-        panic!("No such flag '{}' exists!", name)
-    }
-
-    /// Return the help text.
-    pub fn help(&self) -> String {
-        let mut text: String = format!(
-            "{}\n{}\n\nUSAGE:\n    {}\n\nFLAGS:\n",
-            self.name, self.description, self.usage
-        );
-
-        for flag in &self.flags {
-            text = format!("{}    {}\n", text, flag.format());
-        }
-
-        text
     }
 }
 
-impl Flag {
-    /// Constructor.
-    pub const fn new(name: &'static str) -> Self {
-        Flag {
-            name,
-            long: None,
-            short: None,
-            description: None,
-            has: false,
+#[cfg(test)]
+mod test {
+    BuildArgumentParser! {
+        name: "mock",
+        description: "Mock program",
+        usage: "mock [FLAGS] BINARY [ARGS...]",
+
+        flags: {
+            /// Turn flag 1 on
+            flag1:('1', "--flag1"),
+            /// Turn flag 2 on
+            flag2:('2', "--flag2")
         }
     }
 
-    /// Set the character used as the short name of this flag. E.g. 'c' for '-c'.
-    pub const fn set_short(mut self, short: char) -> Self {
-        self.short = Some(short);
-        self
-    }
+    #[test]
+    fn argument_parsing() {
+        let mut ap = ArgumentParser::default();
+        let args = ["mock", "-1", "--flag2"].map(String::from);
+        ap.process_arguments(&args);
+        assert!(ap.flag1);
+        assert!(ap.flag2);
 
-    /// Set the long name, e.g. "--long". Be sure to include the "--".
-    pub const fn set_long(mut self, long: &'static str) -> Self {
-        self.long = Some(long);
-        self
-    }
+        let mut ap = ArgumentParser::default();
+        let args = ["mock", "-1"].map(String::from);
+        ap.process_arguments(&args);
+        assert!(ap.flag1);
+        assert!(!ap.flag2);
 
-    /// Set the description to be used in the help/usage message.
-    pub const fn set_description(mut self, description: &'static str) -> Self {
-        self.description = Some(description);
-        self
-    }
+        let mut ap = ArgumentParser::default();
+        let args = ["mock", "-12"].map(String::from);
+        ap.process_arguments(&args);
+        assert!(ap.flag1);
+        assert!(ap.flag2);
 
-    /// Check if long name matches the given string, including the "--". Later, the client can call
-    /// `has_match()` which will return true if(but not only if) it had matched.
-    pub fn match_against_long(&mut self, pattern: &str) -> bool {
-        let long = self.long.unwrap_or("");
+        let mut ap = ArgumentParser::default();
+        let args = ["mock", "-21"].map(String::from);
+        ap.process_arguments(&args);
+        assert!(ap.flag1);
+        assert!(ap.flag2);
 
-        if pattern == long {
-            self.has = true;
-            return true;
-        };
-
-        false
-    }
-
-    /// Check if short name matches the given char. Later, the client can call `has_match()` which
-    /// will return true if(but not only if) it had matched.
-    pub fn match_against_short(&mut self, pattern: char) -> bool {
-        if let Some(short) = self.short {
-            if pattern == short {
-                self.has = true;
-                return true;
-            }
-        };
-
-        false
-    }
-
-    /// Returns true if this flag has been matched before.
-    pub const fn has_match(&self) -> bool {
-        self.has
-    }
-
-    /// Make small summary of flag for use in help menu.
-    pub fn format(&self) -> String {
-        let mut text = String::new();
-
-        if let Some(short) = self.short {
-            text = format!("{}-{}", text, short);
-            if self.long.is_some() {
-                text += ", ";
-            }
-        }
-
-        if let Some(long) = self.long {
-            text += long
-        }
-
-        format!("{:20}{}", text, self.description.unwrap())
+        let mut ap = ArgumentParser::default();
+        let args = ["mock", "-2"].map(String::from);
+        ap.process_arguments(&args);
+        assert!(!ap.flag1);
+        assert!(ap.flag2);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,40 +4,27 @@ use std::{
     os::unix::process::CommandExt,
     process::{self, Command},
 };
-
 mod argparser;
-use argparser::{ArgumentParser, Flag};
+
+BuildArgumentParser! {
+    name: "lax",
+    description: "Argument substition utility",
+    usage: "lax [FLAGS] BINARY [ARGS...]",
+
+    flags: {
+        /// Only match directories
+        directories:('d', "--directories"),
+        /// Only match files
+        files: ('f', "--files"),
+        /// Print transformed args to stdout, but don't execute
+        print_only: ('p', "--print-only"),
+        /// Transform matched files to their parent directory
+        file_to_parent: ('D', "--file2parent")
+    }
+}
 
 fn main() {
-    let mut ap = ArgumentParser::new(
-        "lax",
-        "Argument substitution utility",
-        "lax [FLAGS] BINARY [ARGS...]",
-    )
-    .add_flag(
-        Flag::new("directories")
-            .set_description("Only match directories")
-            .set_long("--directories")
-            .set_short('d'),
-    )
-    .add_flag(
-        Flag::new("files")
-            .set_description("Only match files")
-            .set_long("--files")
-            .set_short('f'),
-    )
-    .add_flag(
-        Flag::new("print_only")
-            .set_description("Print transformed args to stdout, but don't execute")
-            .set_long("--print-only")
-            .set_short('p'),
-    )
-    .add_flag(
-        Flag::new("file_to_parent")
-            .set_description("Transform matched files to their parent directory")
-            .set_long("--file2parent")
-            .set_short('D'),
-    );
+    let mut ap = ArgumentParser::default();
 
     let args: Vec<String> = env::args().collect();
     let args = ap.process_arguments(&args);
@@ -47,22 +34,16 @@ fn main() {
         eprintln!("For more information try --help");
         process::exit(1);
     }
-
-    let mut config = lax::Config::new();
-
-    if ap.has("directories") {
-        config.match_with_files = false;
-    }
-    if ap.has("file_to_parent") {
-        config.transform_files_to_dirs = true;
-    }
-    if ap.has("files") {
-        config.match_with_dirs = false;
-    }
-    if !config.match_with_dirs && !config.match_with_files {
+    if ap.files && ap.directories {
         eprintln!("The `-d` and `-f` flag can not be on at the same time. They are incompatible.");
         process::exit(1);
     }
+
+    let config = lax::Config {
+        transform_files_to_dirs: ap.file_to_parent,
+        match_with_files: !ap.directories,
+        match_with_dirs: !ap.files,
+    };
 
     // After this, we only do '@' transformations
     let expander = lax::Expander {
@@ -103,7 +84,7 @@ fn main() {
         }
     };
 
-    if ap.has("print_only") {
+    if ap.print_only {
         print!("{}", args.join(" "));
     } else {
         // Go ahead and run the binary with the transformed arguments
